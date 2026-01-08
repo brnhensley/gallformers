2a5cbb01f73de40663a6dea613e602f94629a77a
jeffdc <jeff@nothoo.com>
2026-01-08 15:48:07 -0500

    exclude v2/ from v1 CI workflow and tooling

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,8 @@ const createJestConfig = nextJest({
 const customJestConfig = {
     // Automatically clear mock calls and instances between every test
     clearMocks: true,
+    // Ignore v2 directory (separate test setup)
+    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/v2/'],
     // The directory where Jest should output its coverage files
     coverageDirectory: '.coverage',
     // A list of paths to modules that run some code to configure or set up the testing framework before each test
