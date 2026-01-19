ba1f5c3aa68d79516c14a750a02190c1e9c0ce85
jeffdc <jeff@nothoo.com>
2026-01-19 16:37:18 -0500

    Move migrations from release_command to entrypoint
    release_command doesn't work with SQLite volumes on Fly.io because
    the release machine gets a forked snapshot of the volume and changes
    don't persist back. Migrations now run in docker-entrypoint.sh on
    the actual app machine before starting litestream.

--- v2/docker-entrypoint.sh
+++ v2/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Docker entrypoint for Gallformers V2
-# Fixes file permissions before starting the app
+# Runs migrations then starts the app with Litestream replication
 
 set -e
 
@@ -9,6 +9,12 @@ set -e
 if [ -d /data ]; then
   chown -R gallformers:gallformers /data
 fi
+
+# Run database migrations before starting the server
+# Note: release_command doesn't work with SQLite volumes on Fly.io because
+# the release machine gets a forked snapshot that doesn't persist changes
+echo "Running database migrations..."
+su-exec gallformers /app/bin/gallformers eval 'Gallformers.Release.migrate()'
 
 # Switch to gallformers user and start litestream with the app
 exec su-exec gallformers litestream replicate -exec "/app/bin/server"
