a8c2917454491e42452f5828a3f75345c79f690d
jeffdc <jeff@nothoo.com>
2026-01-21 11:10:29 -0500

    Add all icon files to static_paths() whitelist
    Fixes favicon and icon files not being served by adding all 29
    icon/config files to the Plug.Static whitelist.

--- lib/gallformers_web.ex
+++ lib/gallformers_web.ex
@@ -19,7 +19,16 @@ defmodule GallformersWeb do
 
   def static_paths,
     do:
-      ~w(assets fonts images branding data favicon.ico favicon.png apple-touch-icon.png robots.txt)
+      ~w(assets fonts images branding data robots.txt
+         favicon.ico favicon.png favicon-16x16.png favicon-32x32.png favicon-96x96.png
+         apple-touch-icon.png apple-icon.png apple-icon-precomposed.png
+         apple-icon-57x57.png apple-icon-60x60.png apple-icon-72x72.png apple-icon-76x76.png
+         apple-icon-114x114.png apple-icon-120x120.png apple-icon-144x144.png
+         apple-icon-152x152.png apple-icon-180x180.png
+         android-icon-36x36.png android-icon-48x48.png android-icon-72x72.png
+         android-icon-96x96.png android-icon-144x144.png android-icon-192x192.png
+         ms-icon-70x70.png ms-icon-144x144.png ms-icon-150x150.png ms-icon-310x310.png
+         manifest.json browserconfig.xml)
 
   def router do
     quote do
