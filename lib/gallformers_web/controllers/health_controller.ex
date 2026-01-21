b07f9dabc6d4f62bd0a0da8de9fabd3d8ed017ae
jeffdc <jeff@nothoo.com>
2026-01-21 10:47:44 -0500

    Improve deployment health check for zero-downtime deploys
    - Health check now verifies database connectivity before returning healthy
    - Increase grace period from 10s to 30s to allow more startup time

--- lib/gallformers_web/controllers/health_controller.ex
+++ lib/gallformers_web/controllers/health_controller.ex
@@ -1,9 +1,26 @@
 defmodule GallformersWeb.HealthController do
   use GallformersWeb, :controller
 
+  alias Gallformers.Repo
+
   def check(conn, _params) do
-    conn
-    |> put_resp_content_type("text/plain")
-    |> send_resp(200, "ok")
+    case check_database() do
+      :ok ->
+        conn
+        |> put_resp_content_type("text/plain")
+        |> send_resp(200, "ok")
+
+      {:error, reason} ->
+        conn
+        |> put_resp_content_type("text/plain")
+        |> send_resp(503, "database unavailable: #{inspect(reason)}")
+    end
+  end
+
+  defp check_database do
+    case Repo.query("SELECT 1") do
+      {:ok, _result} -> :ok
+      {:error, reason} -> {:error, reason}
+    end
   end
 end
